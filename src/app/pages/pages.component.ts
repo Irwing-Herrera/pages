ccfebea646da304f3090bb0db543891ff148db13
Irwing <irwing-herrera-2b@hotmail.com>
2019-03-31 11:58:25 -0600

    Agregación de seccion 2 y seccion 3

--- src/app/pages/pages.component.ts
+++ src/app/pages/pages.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+declare const $;
 
 @Component({
   selector: 'app-pages',
@@ -7,9 +8,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PagesComponent implements OnInit {
 
-  constructor() { }
+  constructor() {
+    
+    window.onscroll = function () { changueColorNav() };
+    function changueColorNav() {
+      let nav = document.getElementById("sideNav");
+      if (document.body.scrollTop > 56 || document.documentElement.scrollTop > 56) {
+        nav.style.transition = "background-color 500ms linear";
+        nav.style.backgroundColor = "rgba(0,0,0,0.6)";
+      } else {
+        nav.style.backgroundColor = "rgba(0,0,0,0)";
+        // nav.removeAttribute("style")
+      }
+    }
+  }
 
   ngOnInit() {
   }
 
+  
 }
